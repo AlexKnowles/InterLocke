59bec9f3fe9543ded62d175c6354800258c0bbfa
Lee Martin <lee.martin@pliminus.co.uk>
2021-06-12 11:56:52 +0100

    Added: Paralax water background

---
+++ Assets/Scripts/Paralax.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralax : MonoBehaviour
+{
+    private float length, startpos;
+    public GameObject Camera;
+    public float ParalaxEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float offset = Camera.transform.position.x * (1 - ParalaxEffect);
+        float distance = Camera.transform.position.x * ParalaxEffect;
+        
+        transform.position = new Vector2(startpos + distance, transform.position.y);
+        if (offset > startpos + length) startpos += length;
+        else if (offset < startpos - length) startpos -= length;
+    }
+}
