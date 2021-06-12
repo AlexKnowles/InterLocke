1aa1d25560294ff173c06ae4a6e0b65ea6cda23b
Lee Martin <lee.martin@pliminus.co.uk>
2021-06-12 16:40:42 +0100

    Added: Infinite generation of banks

--- Assets/Scripts/BankGenerator.cs
+++ Assets/Scripts/BankGenerator.cs
@@ -8,36 +8,57 @@ public class BankGenerator : MonoBehaviour
 {
     public GameObject BankPrefab;
     public GameObject CameraObject;
-    
+
     private Camera Camera;
 
     private List<GameObject> _banksList = new List<GameObject>();
+
+    private const float BANK_PREFAB_WIDTH = 20f;
+
+    private int banksToGenerate = 0;
+    private int offsetAhead = 10;
     
     void Start()
     {
         Camera = CameraObject.GetComponent<Camera>();
-        //This is dumb. Lets do better.
-        for (int i = -10;i < 10;i++){
-            var newBank = Instantiate(BankPrefab, transform);
-            newBank.transform.position = new Vector2(i * 20, 0);
-            _banksList.Add(newBank);
+        //Generating some banks to start
+        for (int i = -10;i < offsetAhead;i++){
+            GenerateNewBank(i);
         }
     }
 
 
     void Update()
     {
+        GenerateNewBanks();
         DestroyOutOfFrameBehindBanks();
+    }
+
+    private void GenerateNewBanks()
+    {
+        while (banksToGenerate > 0){
+            banksToGenerate--;
+            GenerateNewBank(offsetAhead);
+            offsetAhead++;
+        }
+    }
+
+    private void GenerateNewBank(int offsetAhead)
+    {
+            var newBank = Instantiate(BankPrefab, transform);
+            newBank.transform.position = new Vector2(offsetAhead * BANK_PREFAB_WIDTH, 0);
+            _banksList.Add(newBank);
     }
 
     private void DestroyOutOfFrameBehindBanks() {
         var banksToRemove = _banksList.Where(x => ObjectAheadOrInFrame(x)).ToList();
         banksToRemove.ForEach(x => Destroy(x));
+        banksToGenerate += banksToRemove.Count();
         _banksList.RemoveAll(x => banksToRemove.Contains(x));
     }
 
     private bool ObjectAheadOrInFrame(GameObject gameObject){
         var objectCameraX = Camera.WorldToViewportPoint(gameObject.transform.position).x;
-        return gameObject.transform.position.x < CameraObject.transform.position.x &&  (objectCameraX > 1 || objectCameraX < 0); 
+        return gameObject.transform.position.x < CameraObject.transform.position.x &&  (objectCameraX > 1 || objectCameraX < -1); 
     }
 }
