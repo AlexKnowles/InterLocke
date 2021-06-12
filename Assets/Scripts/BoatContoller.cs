4d2dcca5aec36ccf12f7948d08fce9ff911515f6
Alex Knowles <git@alexknowles.info>
2021-06-12 14:17:53 +0100

    More boat tuning

--- Assets/Scripts/BoatContoller.cs
+++ Assets/Scripts/BoatContoller.cs
@@ -1,13 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BoatContoller : MonoBehaviour
 {
-	public float accel = 10f;
 	public float turnSpeed = 3f;
-	public float maxSpeed = 8f;
-	
+	public float ForceScale = 2f;
+	public Vector2 Position = new Vector2(0, -1);
+
 	private Rigidbody2D rigidbody;
 
 	private void Start()
@@ -18,27 +16,15 @@ public class BoatContoller : MonoBehaviour
 
     void Update()
 	{
-
-		////if up pressed
-		//if (Input.GetAxis("Vertical") > 0)
-		//{
-		//	//add force
-		//	rigidbody.AddRelativeForce(Vector2.up * accel);
-
-		//	//if we are going too fast, cap speed
-		//	if (rigidbody.velocity.magnitude > maxSpeed)
-		//	{
-		//		rigidbody.velocity = rigidbody.velocity.normalized * maxSpeed;
-		//	}
-		//}
-
-		//if right/left pressed add torque to turn
-		if (Input.GetAxis("Vertical") != 0)
+        //if right/left pressed add torque to turn
+        if (Input.GetAxis("Vertical") != 0)
 		{
-			//scale the amount you can turn based on current velocity so slower turning below max speed
-			float scale = Mathf.Lerp(0f, turnSpeed, (rigidbody.velocity.magnitude / maxSpeed));
-			//axis is opposite what we want by default
-			rigidbody.AddTorque(-Input.GetAxis("Vertical") * scale * Time.deltaTime);
+			float scale = Mathf.Lerp(0f, turnSpeed, rigidbody.velocity.magnitude);
+
+			float torqueOverTime = -Input.GetAxis("Vertical") * scale * Time.deltaTime;
+
+			rigidbody.AddRelativeForce(Vector2.right * torqueOverTime * ForceScale);
+			rigidbody.AddTorque(torqueOverTime*-1);
 		}
 	}
 }
