c56f07df6e3d8eb7b662f60d26561c7b9cfe68d2
Alex Knowles <git@alexknowles.info>
2021-06-12 10:49:05 +0100

    Better Rope

--- Assets/Scripts/Rope.cs
+++ Assets/Scripts/Rope.cs
@@ -1,12 +1,11 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(HingeJoint2D))]
 public class Rope : MonoBehaviour
 {
+    public GameObject EndObject1;
+    public GameObject EndObject2;
     public GameObject LinkPrefab;
     public int NumberOfLinks = 7;
-    public GameObject Horse;
 
     private void Start()
     {
@@ -15,21 +14,30 @@ public class Rope : MonoBehaviour
 
     private void GenerateRope()
     {
-        Rigidbody2D lastLinkObject = GetComponent<Rigidbody2D>();
+        Rigidbody2D lastLinkObject = CreateLink(EndObject1.GetComponent<Rigidbody2D>());
+
+        ConnectToEndObject(EndObject1, lastLinkObject);
 
         for (var currentLinkId = 0; currentLinkId < NumberOfLinks; currentLinkId++)
         {
-            GameObject newLinkObject = Instantiate(LinkPrefab, transform);
-            newLinkObject.name = ("link_" + currentLinkId);
-
-            HingeJoint2D newLinkjoint = newLinkObject.GetComponent<HingeJoint2D>();
-            newLinkjoint.connectedBody = lastLinkObject;
-
-            lastLinkObject = newLinkObject.GetComponent<Rigidbody2D>();
-
+            lastLinkObject = CreateLink(lastLinkObject);
         }
 
-        HingeJoint2D norseBody = Horse.GetComponent<HingeJoint2D>();
-        norseBody.connectedBody = lastLinkObject;
+        ConnectToEndObject(EndObject2, lastLinkObject);
+    }
+    private void ConnectToEndObject(GameObject endObject, Rigidbody2D lastRigidBody)
+    {
+        HingeJoint2D endObjectHinge = endObject.GetComponent<HingeJoint2D>();
+        endObjectHinge.connectedBody = lastRigidBody;
+    }
+
+    private Rigidbody2D CreateLink(Rigidbody2D lastRigidBody)
+    {
+        GameObject newLinkObject = Instantiate(LinkPrefab, transform);
+
+        HingeJoint2D newLinkjoint = newLinkObject.GetComponent<HingeJoint2D>();
+        newLinkjoint.connectedBody = lastRigidBody;
+
+        return newLinkObject.GetComponent<Rigidbody2D>();
     }
 }
