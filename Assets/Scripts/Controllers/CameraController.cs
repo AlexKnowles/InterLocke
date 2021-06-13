5fb98cf4b48298e0d945bc8d5e51fe5baf67035f
Alex Knowles <git@alexknowles.info>
2021-06-13 15:21:01 +0100

    Fixed cameras y

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -26,12 +26,12 @@ public class CameraController : MonoBehaviour
         float desiredX = (distanceBetweenTargets / 2) * ((Target2.position.x - Target1.position.x) / distanceBetweenTargets);
         float desiredY = (distanceBetweenTargets / 2) * ((Target2.position.y - Target1.position.y) / distanceBetweenTargets);
 
-        Vector3 centerBetweenTargets = new Vector3(Target1.position.x + desiredX, Target1.position.y + desiredY, transform.position.z);
+        Vector3 centerBetweenTargets = new Vector3(Target1.position.x + desiredX, transform.position.y, transform.position.z);
         float hypotenuse = (distanceBetweenTargets / 2);
 
         transform.position = Vector3.Lerp(transform.position, centerBetweenTargets, (hypotenuse/ CatchupSlowFactor) * Time.deltaTime);
 
-        camera.orthographicSize = Mathf.Max((hypotenuse * 0.776f), 8); 
+        camera.orthographicSize = Mathf.Max((hypotenuse), 8); 
     }
 
     private void StartGame()
